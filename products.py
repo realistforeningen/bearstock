76486b7e9ded0ade078368a3aa51dca09df19322
Magnus Holm <judofyr@gmail.com>
2015-10-07 14:19:42 +0200

    Add a few more products

--- products.py
+++ products.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 products = [
 	{
 		"code": "FYPA",
@@ -9,12 +10,30 @@ products = [
 		"code": "FYBY",
 		"name": "Frydenlund Bayer Tapp",
 		"base_price": 30,
-		"tags": ["Tapp", "Frydenlund"],
+		"tags": ["Tapp", "Frydenlund", "Bayer"],
 	},
 	{
 		"code": "FYPL",
 		"name": "Frydenlund Pils Tapp",
 		"base_price": 30,
 		"tags": ["Tapp", "Frydenlund", "Pils"],
+	},
+	{
+		"code": "PUHW",
+		"name": "Paulaner Hefe-Weissbier",
+		"base_price": 30,
+		"tags": ["Paulaner", u"Hveteøl"],
+	},
+	{
+		"code": "FDAI",
+		"name": "Founders All Day IPA",
+		"base_price": 30,
+		"tags": ["Founders", "IPA"],
+	},
+	{
+		"code": "CRGI",
+		"name": "Crabbies Ginger Beer",
+		"base_price": 30,
+		"tags": ["Crabbies", "Ginger"],
 	},
 ]
