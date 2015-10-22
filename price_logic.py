73204a076215905733af00f7046fe07bb8e4fabc
Eivind Storm Aarnæs <eiv.s.aa@gmail.com>
2015-10-22 19:17:37 +0200

    Decrease ported.

--- price_logic.py
+++ price_logic.py
@@ -5,6 +5,11 @@ BASE_PARAMETERS = {
     # expected sales parameters
     'ex_periods': 12,  # strictly positive number
     'ex_lookback': 12,  # strictly positive or None
+    # adjustment parameters
+    'decrease_percentage': 0.50,
+    'ad_acqu_weight': 1,
+    'ad_prev_adjust_weight': 10,
+    'ad_past_sales_weight': 20,
 }
 
 ## logic
@@ -47,16 +52,49 @@ class PriceLogic:
         params : dict, optional
             Parameters.
         """
+        total_products = products_left + sum(
+            tuple((data['sold_units'] for data in price_data))
+        )
         self.products[code] = {
             'base_price': base_price,
+            'prev_adj': (price_data[-1]['adjustment'] if price_data else 0),
+            'fraction_left': (
+                (float(products_left)/total_products)
+                if total_products > 0 and products_left >= 0 else 1.0
+            ),
             'price_data': price_data,
             'p': params,
         }
         self.products[code]['expected'] = self._expected_sales(code)
         self.products[code]['adjustment'] = self._compute_adjustment(code)
+        print 'Adjustment[%s] = %.2f (prev: %.2f)' % (code, self.products[code]['adjustment'], self.products[code]['prev_adj'])
 
     def _compute_adjustment(self, code):
-        return 0  # TODO
+        product = self.products[code]
+        params = product['p']
+        p = params['decrease_percentage']
+        ## read parameters
+        w = (
+            params['ad_acqu_weight'],
+            params['ad_prev_adjust_weight'],
+            params['ad_past_sales_weight'],
+        )
+        weight_abs_sum = sum(map(abs, w))
+        ## periods since last purchase
+        delta_purchase = len(product['price_data'])
+        for pid, data in enumerate(reversed(product['price_data'])):
+            if data['sold_units'] > 0:
+                delta_purchase = pid
+                break
+        ## compute decrease
+        decrease_by = p*(
+            w[0]*product['base_price'] +
+            -w[1]*product['prev_adj'] +
+            w[2]*delta_purchase
+        )/(weight_abs_sum if weight_abs_sum != 0 else 1)
+        decrease_by *= product['fraction_left']/max(1, product['expected'])
+
+        return -decrease_by
 
     def finalize(self):
         """Finalize the price calculations.
@@ -70,7 +108,7 @@ class PriceLogic:
         adjustments = {}
         for code in self.products:
             if 'adjustment' in self.products[code]:
-                adjustments[code] = self.products[code]['adjustment']
+                adjustments[code] = round(self.products[code]['adjustment'])
         return adjustments
 
     def _expected_sales(self, code):
