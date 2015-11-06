a744a78aef4f5f5a8f23a341ebd552bfda0821d4
Eivind Storm Aarnæs <eiv.s.aa@gmail.com>
2015-11-06 21:13:31 +0100

    Counting orders & summing sale prices.

--- stock.py
+++ stock.py
@@ -88,6 +88,9 @@ class Database:
     def insert_buyer(self, name):
         return self.insert("buyers", name=name)
 
+    def read_all_orders(self):
+        return self.e('SELECT * FROM orders')
+
     # How much money is on our own account?
     def stock_account(self):
         return self.e('SELECT SUM(relative_cost) FROM orders').fetchone()[0] or 0
