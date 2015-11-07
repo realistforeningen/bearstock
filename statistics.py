a787e13736992e1dd3e600b4ea901b8631fc26c9
Eivind Storm Aarnæs <eiv.s.aa@gmail.com>
2015-11-07 14:37:07 +0100

    Statistics done.

--- statistics.py
+++ statistics.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+from stock import Database
+
 def get_top_bot(count):
     """Get ``count`` top and bottom traders.
 
@@ -10,7 +12,7 @@ def get_top_bot(count):
 
     Returns
     -------
-    topbot : dict
+    top_bot : dict
         A dict containing three keys: ``count``, ``top``, and ``bottom``.
 
          * ``count`` is the number of top and bottom traders.
@@ -19,5 +21,43 @@ def get_top_bot(count):
             * ``id``: Trader ID.
             * ``turnover``: Total turnover for the trader.
             * ``profit``: Profit relative to base price of products bought.
+
+        If there are fewer traders than ``count``, the lists are shorter than
+        ``count``.
     """
-    pass
+    # get data from DB
+    db = Database.default()
+    orders = db.read_all_orders()
+    # parse orders
+    traders = {}  # list of traders
+    for order in orders:
+        # Order fields
+        #   oid       : order id
+        #   bid       : buyer id
+        #   code      : product code
+        #   pid       : price id
+        #   rel_cost  : relative cost from product base price
+        #   abs_cost  : absolut cost the buyer paid
+        #   timestamp : order timestamp
+        oid, bid, code, pid, rel_cost, abs_cost, timestamp = order
+        # add data
+        if bid not in traders:
+            traders[bid] = {
+                'id': bid,
+                'turnover': 0,
+                'profit': 0,
+            }
+        traders[bid]['turnover'] += abs_cost
+        traders[bid]['profit'] += rel_cost
+    # construct the list
+    traders = sorted(traders.values(), key=(lambda trader: trader['profit']))
+    # data!
+    return {
+        'count': min(count, len(traders)),
+        'top': tuple(traders[0:count]),
+        'bottom': tuple(traders[-count:]),
+    }
+
+if __name__ == '__main__':
+    from pprint import pprint as pp
+    pp(get_top_bot(5))
