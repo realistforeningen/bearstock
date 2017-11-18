67aea75e1f058cc45c912a4bade4aa0219d1f1e5
Magnus Holm <judofyr@gmail.com>
2017-11-18 19:42:28 +0100

    More tweaks

--- src/bearstock/price_logic_table.py
+++ src/bearstock/price_logic_table.py
@@ -122,15 +122,15 @@ class PriceLogic(PriceLogicBase):
 
         product = self.products[code]
         current_price = product.current_price
-        units_sold = sum(product.timeline.sales[-15:])
+        units_sold = sum(product.timeline.sales[-30:])
 
-        base_adjustment = max(1, current_price - product.base_price)
-        #base_adjustment = 1
+        #base_adjustment = max(1, current_price - product.base_price)
+        base_adjustment = 2
 
         if units_sold == 0:
-            return  -3*base_adjustment
+            return  -2*base_adjustment
         if units_sold <= 1:
-            return -2*base_adjustment
+            return -1*base_adjustment
         if units_sold > 1:
             return 3*base_adjustment
         if units_sold > 5:
